679d71b43b4cb68d64dadda1cc15ae4946b9a565
JohnDoe2991 <24862620+JohnDoe2991@users.noreply.github.com>
2025-03-29 19:37:28 +0100

    add keymaps to use git status and log easier

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -8,4 +8,8 @@ return {
   opts = {
     graph_style = 'unicode',
   },
+  keys = {
+    { '<leader>gg', '<cmd>Neogit<cr>', desc = 'Neogit project status' },
+    { '<leader>gl', '<cmd>Neogit log<cr>', desc = 'Neogit log graph' },
+  },
 }
