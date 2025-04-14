3a845b370b16cc36b9d35851f21525d146560ec4
JohnDoe2991 <24862620+JohnDoe2991@users.noreply.github.com>
2025-04-14 10:13:41 +0200

    add keymaps to overseer

--- lua/plugins/overseer.nvim.lua
+++ lua/plugins/overseer.nvim.lua
@@ -1,4 +1,8 @@
 return {
   'stevearc/overseer.nvim',
   opts = {},
+  keys = {
+    { '<leader>tt', '<cmd>OverseerRun<cr>', desc = 'Run task' },
+    { '<leader>to', '<cmd>OverseerToggle<cr>', desc = 'Show task outputs' },
+  },
 }
