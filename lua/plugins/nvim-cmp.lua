c1de413c16863d442895ae985c79a8195d67b6ac
JohnDoe2991 <24862620+JohnDoe2991@users.noreply.github.com>
2025-04-14 14:15:19 +0200

    enable auto complete for command mode and search

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,6 +1,6 @@
 return { -- Autocompletion
   'hrsh7th/nvim-cmp',
-  event = 'InsertEnter',
+  event = { 'InsertEnter', 'CmdlineEnter' },
   dependencies = {
     -- Snippet Engine & its associated nvim-cmp source
     {
@@ -20,11 +20,29 @@ return { -- Autocompletion
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-nvim-lsp-signature-help',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/cmp-buffer',
   },
   config = function()
     -- See `:help cmp`
     local cmp = require 'cmp'
     local luasnip = require 'luasnip'
+    local tabCompleteFunction = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        local entries = cmp.get_entries()
+        local selected = cmp.get_selected_entry()
+        if selected or #entries == 1 then
+          -- Eintrag ausgewählt oder nur ein Eintrag vorhanden = füge ein
+          cmp.confirm { select = true }
+        else
+          -- Sonst vervollständige bis zum gemeinsamen Präfix
+          -- WICHTIG: damit die Funktion das tut, darf kein Eintrag ausgewählt sein!
+          cmp.complete_common_string()
+        end
+      else
+        fallback()
+      end
+    end, { 'i', 'c' })
     luasnip.config.setup {}
 
     cmp.setup {
@@ -46,67 +64,9 @@ return { -- Autocompletion
         disallow_symbol_nonprefix_matching = false,
       },
 
-      -- For an understanding of why these mappings were
-      -- chosen, you will need to read `:help ins-completion`
-      --
-      -- No, but seriously. Please read `:help ins-completion`, it is really good!
       mapping = cmp.mapping.preset.insert {
-
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<Tab>'] = function(fallback)
-          if cmp.visible() then
-            local entries = cmp.get_entries()
-            local selected = cmp.get_selected_entry()
-            if selected or #entries == 1 then
-              -- Eintrag ausgewählt oder nur ein Eintrag vorhanden = füge ein
-              cmp.confirm { select = true }
-            else
-              -- Sonst vervollständige bis zum gemeinsamen Präfix
-              -- WICHTIG: damit die Funktion das tut, darf kein Eintrag ausgewählt sein!
-              cmp.complete_common_string()
-            end
-          else
-            fallback()
-          end
-        end,
-        -- ['<Tab>'] = cmp.mapping.confirm { select = true },
-        -- ['<Tab>'] = cmp.mapping(function(fallback)
-        --   if cmp.visible() then
-        --     cmp.select_next_item()
-        --   elseif has_words_before() then
-        --     cmp.complete()
-        --   else
-        --     fallback()
-        --   end
-        -- end, { 'i', 's' }),
-
-        -- Manually trigger a completion from nvim-cmp.
-        --  Generally you don't need this, because nvim-cmp will display
-        --  completions whenever it has completion options available.
+        ['<Tab>'] = tabCompleteFunction,
         ['<C-Space>'] = cmp.mapping.complete {},
-
-        -- Think of <c-l> as moving to the right of your snippet expansion.
-        --  So if you have a snippet that's like:
-        --  function $name($args)
-        --    $body
-        --  end
-        --
-        -- <c-l> will move you to the right of each of the expansion locations.
-        -- <c-h> is similar, except moving you backwards.
-        ['<C-l>'] = cmp.mapping(function()
-          if luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          end
-        end, { 'i', 's' }),
-        ['<C-h>'] = cmp.mapping(function()
-          if luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          end
-        end, { 'i', 's' }),
-
-        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       },
       sources = {
         {
@@ -120,5 +80,30 @@ return { -- Autocompletion
         { name = 'nvim_lsp_signature_help' },
       },
     }
+    local cmdline_mappings = cmp.mapping.preset.cmdline()
+    cmdline_mappings['<Tab>'] = tabCompleteFunction
+    -- Set up command-line completion (:)
+    cmp.setup.cmdline(':', {
+      mapping = cmdline_mappings,
+      completeopt = 'menu,menuone,noinsert',
+      sources = cmp.config.sources({
+        { name = 'path' },
+      }, {
+        {
+          name = 'cmdline',
+          option = {
+            ignore_cmds = { 'Man', '!' },
+          },
+        },
+      }),
+    })
+
+    -- Set up search completion (/)
+    cmp.setup.cmdline('/', {
+      mapping = cmdline_mappings,
+      sources = {
+        { name = 'buffer' },
+      },
+    })
   end,
 }
