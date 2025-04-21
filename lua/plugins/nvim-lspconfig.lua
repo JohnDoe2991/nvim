35eb6ba6004dcbb533896facd680384040b722aa
JohnDoe2991 <24862620+JohnDoe2991@users.noreply.github.com>
2025-04-21 10:32:14 +0200

    add keymap for lsp info

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -65,6 +65,8 @@ return {
         --  For example, in C this would take you to the header.
         map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+        map('<leader>i', vim.lsp.buf.hover, 'LSP info for current selection')
+
         -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
         ---@param client vim.lsp.Client
         ---@param method vim.lsp.protocol.Method
