f8748c8a714a3a8fdfa78a65117990137c9b3c92
JohnDoe2991 <24862620+JohnDoe2991@users.noreply.github.com>
2025-05-20 17:31:14 +0200

    feat: play sound on notification

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -4,5 +4,29 @@ return {
     local notify = require 'notify'
     vim.opt.termguicolors = true
     vim.notify = notify
+    notify.setup {
+      on_open = function(win)
+        io.write '\7'
+      end,
+      background_colour = 'NotifyBackground',
+      fps = 30,
+      icons = {
+        DEBUG = '',
+        ERROR = '',
+        INFO = '',
+        TRACE = '✎',
+        WARN = '',
+      },
+      level = 2,
+      minimum_width = 50,
+      render = 'default',
+      stages = 'fade_in_slide_out',
+      time_formats = {
+        notification = '%T',
+        notification_history = '%FT%T',
+      },
+      timeout = 5000,
+      top_down = true,
+    }
   end,
 }
