14d83c58b8624201818a0698ebea623094385cbc
JohnDoe2991 <24862620+JohnDoe2991@users.noreply.github.com>
2025-06-07 20:27:13 +0200

    feat: add a workaround for submodule support in neogit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -9,6 +9,19 @@ return {
     graph_style = 'unicode',
   },
   keys = {
+    -- workaround for submodule support, see: https://github.com/NeogitOrg/neogit/issues/870
+    {
+      'n',
+      '<leader>gg',
+      function()
+        local file_dir = vim.fn.expand '%:h'
+        local exec_result = vim.system({ 'git', 'rev-parse', '--show-toplevel' }, { text = true, cwd = file_dir }):wait()
+        local root = vim.fn.trim(exec_result.stdout)
+        require('neogit').open { kind = 'replace', cwd = root }
+      end,
+      desc = 'Neogit status',
+      silent = true,
+    },
     { '<leader>gg', '<cmd>Neogit<cr>', desc = 'Neogit project status' },
     { '<leader>gl', '<cmd>Neogit log<cr>', desc = 'Neogit log graph' },
   },
