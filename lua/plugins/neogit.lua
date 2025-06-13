2253fc018a2bd88f4c817efd0341aeed8d0b3f4a
JohnDoe2991 <24862620+JohnDoe2991@users.noreply.github.com>
2025-06-13 16:33:04 +0200

    fix: graph style

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -6,7 +6,7 @@ return {
     'nvim-telescope/telescope.nvim', -- optional
   },
   opts = {
-    graph_style = 'unicode',
+    graph_style = 'kitty',
   },
   keys = {
     -- workaround for submodule support, see: https://github.com/NeogitOrg/neogit/issues/870
@@ -16,12 +16,11 @@ return {
         local file_dir = vim.fn.expand '%:h'
         local exec_result = vim.system({ 'git', 'rev-parse', '--show-toplevel' }, { text = true, cwd = file_dir }):wait()
         local root = vim.fn.trim(exec_result.stdout)
-        require('neogit').open { kind = 'replace', cwd = root }
+        require('neogit').open { kind = 'tab', cwd = root }
       end,
       desc = 'Neogit status',
       silent = true,
     },
-    { '<leader>gg', '<cmd>Neogit<cr>', desc = 'Neogit project status' },
     { '<leader>gl', '<cmd>Neogit log<cr>', desc = 'Neogit log graph' },
   },
 }
