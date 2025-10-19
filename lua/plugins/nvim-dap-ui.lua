1ab048637333dada5b808b7d4e96b9424e7ef470
JohnDoe2991 <24862620+JohnDoe2991@users.noreply.github.com>
2025-10-19 14:17:47 +0200

    feat(dapui) improve key bindings

--- lua/plugins/nvim-dap-ui.lua
+++ lua/plugins/nvim-dap-ui.lua
@@ -3,7 +3,15 @@ return {
   dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' },
   config = function()
     local dap, dapui = require 'dap', require 'dapui'
-    dapui.setup()
+    dapui.setup {
+      element_mappings = {
+        stacks = {
+          -- make stacktrace open element if selected
+          open = '<CR>',
+          expand = 'o',
+        },
+      },
+    }
     dap.listeners.before.attach.dapui_config = function()
       dapui.open()
     end
@@ -17,4 +25,16 @@ return {
       dapui.close()
     end
   end,
+  keys = {
+    {
+      '<leader>dd',
+      function()
+        require('dapui').toggle()
+      end,
+      desc = 'Toggle Debug Overlay',
+    },
+    { '<leader>db', '<cmd>DapToggleBreakpoint<cr>', desc = 'Toggle Breakpoint' },
+    { '<leader>dq', '<cmd>DapTerminate<cr>', desc = 'Stop Debugging' },
+    { '<leader>ds', '<cmd>DapContinue<cr>', desc = 'Start/Continue Debugging' },
+  },
 }
